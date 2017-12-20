98ad21f42ad894cf297cdc89888fad47ef08e32a
piotr.rawski@gmail.com <piotrrawski@macbook-air-piotr.home>
2017-12-20 15:21:08 +0100

    initials

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+//Konfiguracja Webpack
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: ['whatwg-fetch', './js/app.jsx'],
+    output: {
+        filename: "./js/out.js"
+    },
+    devServer: {
+        inline: true,
+        contentBase: './',
+        port: 3001
+    },
+    watch: true,
+    devtool: 'source-map',
+
+    module: {
+        loaders: [{
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'stage-2', 'react']
+            }
+        }]
+    },
+
+    // plugins: [
+    //     new UglifyJsPlugin({
+    //         sourceMap: true,
+    //         exclude: /node_modules/
+    //     })
+    // ]
+};
